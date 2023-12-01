e827a0219bf03f2d42eb76d4ea9382be386504c8
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2023-12-01 03:30:03 -0800

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - prog creates a file
  * @filename: filename.
- * @text_content: content written in the file
+ * @text_content: content writed in the file
  *
  * Return: 1 if it success, -1 if it fails.
  */
@@ -16,15 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0000);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0000);
 
 	if (file_d == -1)
 		return (-1);
 
 	if (!text_content)
-		text_content = -;
+		text_content=--;
 
-	for (nletters = 0; text_content(nletters); nletters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
 	rwr = write(file_d, text_content, nletters);
